f3423b8be0ba1b4b1e9b1b366f7613d696e7eb24
ToNoey <vonmane001@gmail.com>
2019-10-26 17:03:24 +0700

    SCG page route

--- pages/scg.js
+++ pages/scg.js
@@ -1,14 +1,32 @@
+import { useEffect } from 'react'
 import styled from 'styled-components'
-import { Button } from 'react-bootstrap'
 
 const Wrapper = styled('div')`
-  background-color: red;
+  margin: auto;
+  width: 100%;
+  max-width: 600px;
+  display: flex;
+  align-items: center;
+  padding-top: 150px;
+
+  img {
+    width: 100%;
+    height: auto;
+    display: block;
+  }
 `
 
-const ScgPages = () => (
-  <Wrapper>
-    <Button>ssss</Button>
-  </Wrapper>
-)
+const ScgPages = () => {
+
+  useEffect(() => {
+
+  }, [])
+
+  return (
+    <Wrapper>
+      <img src='/static/images/Siam_Cement_Group_Logo.png' />
+    </Wrapper>
+  )
+}
 
 export default ScgPages
